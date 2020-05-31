bf261be13cfcd7fc1397b4a7740c680c97dd6a74
Frac7 <francescacella96@gmail.com>
2020-05-31 19:00:28 +0200

    UI done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Layout, Input, Typography, Row, Col, Space } from 'antd';
+
+const { Header, Content } = Layout;
+const { Title, Text } = Typography;
+const { Search } = Input;
+
+const StyledTitle = styled(Title)`
+  color: white !important;
+`
+
+const App = ({ web3 }) => {
+    const [address, setAddress] = useState(null);
+    const [balance, setBalance] = useState(0);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    return (
+        <Layout>
+            <Header>
+                <StyledTitle>Address {address} information</StyledTitle>
+            </Header>
+            <Content>
+                <Row justify="center" align="middle">
+                    <Col offset={1} span={4}>
+                        <Space direction="vertical">
+                            <Text>Write an address to know its balance</Text>
+                            {error ? <Text strong type="danger">{error.message}</Text> : <Text strong>{`Balance: ${balance} ETH`}</Text>}
+                        </Space>
+                    </Col>
+                    <Col offset={1} span={6}>
+                        <Search
+                            loading={isLoading}
+                            placeholder="Address"
+                            enterButton
+                            onSearch={(value) => {
+                                try {
+                                    setIsLoading(true);
+                                    web3.eth.getBalance(value, (err,res) => {
+                                        setBalance(web3.utils.fromWei(res, 'ether'));
+                                        setAddress(value);
+                                        setIsLoading(false);
+                                    });
+                                    setError(null); //reset previous error
+                                } catch (e) {
+                                    setError(e);
+                                    setAddress(null); //reset previous address
+                                    setIsLoading(false);
+                                }
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Content>
+        </Layout>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import Web3 from 'web3';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import 'antd/dist/antd.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (typeof window.web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(window.web3.currentProvider);
+} else {
+    console.log('No web3? You should consider trying MetaMask!')
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
+
+window.ethereum.enable();
+
+ReactDOM.render(<App web3={window.web3} />, document.getElementById('root'))
